539d2adfbaa6b8559a72c38a35ba47722d10e43f
Guillaume Leclerc <leclerc@mit.edu>
2020-09-29 11:17:07 -0400

    Clean logs and check if submodule was cloned

--- serving/start_inference_server.sh
+++ serving/start_inference_server.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
+# Reading arguments
 MODEL_WEIGHTS_PATH=$(realpath "$1")
 MODEL_CODE_PATH=$(realpath "$2")
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TORCHSERVER_FOLDER="$DIR/serve/docker"
 
 TEMP_FOLDER="/tmp/synthetic-sandbox/serve"
 CONTAINER_MODEL_NAME="sandbox-model"
@@ -9,25 +12,36 @@ CONTAINER_MODEL_WEIGHTS_PATH="/home/model-server/examples/image_classifier/weigh
 CONTAINER_CODE_PATH="/home/model-server/examples/image_classifier/code.py"
 CONTAINER_STORE_PATH="/home/model-server/model-store"
 
+
+# Checking arguments and environment
 if [[ ! -f "$MODEL_WEIGHTS_PATH" ]]
 then
-    echo "[ERROR: $MODEL_WEIGHTS_PATH not found]";
+    echo "[ERROR] $MODEL_WEIGHTS_PATH not found";
     exit 1;
 else
-    echo "[Using weights: $MODEL_WEIGHTS_PATH]";
+    echo "[INFO] Using weights: $MODEL_WEIGHTS_PATH";
 fi
 
 if [[ ! -f "$MODEL_CODE_PATH" ]]; then
-    echo "[ERROR: $MODEL_CODE_PATH not found]";
+    echo "[ERROR] $MODEL_CODE_PATH not found";
     exit 1;
 else
-    echo "[Using architecture: $MODEL_CODE_PATH]";
+    echo "[INFO] Using architecture: $MODEL_CODE_PATH";
 fi
 
-echo "[Cleaning previous runs]"
+if [[ ! -d "$TORCHSERVER_FOLDER" ]]; then
+    echo "[ERROR] torchserve not found";
+    echo "[INFO] expected folder $TORCHSERVER_FOLDER not found";
+    echo "[INFO] You may have forgotten to checkout git submodules";
+    exit 1;
+else
+    echo "[INFO] Using torchserve docker folder: $TORCHSERVER_FOLDER";
+fi
+
+echo "[INFO] Cleaning previous runs"
 rm -rf $TEMP_FOLDER
 
-echo "[Building torchserve]"
+echo "[INFO] Building torchserve"
 cd "$DIR/serve/docker"
 DOCKER_BUILDKIT=1 docker build --file Dockerfile --network=host --build-arg BASE_IMAGE=nvidia/cuda:10.1-cudnn7-runtime-ubuntu18.04 -t torchserve:latest .
 
@@ -36,12 +50,12 @@ mkdir -p $TEMP_FOLDER
 CONTAINER_ID=$(docker run -d --rm -p 8080:8080 -p 8081:8081 --user $(id -u):$(id -g) --network=host -v $TEMP_FOLDER:/home/model-server/model-store -v $DIR/serve/examples:/home/model-server/examples  torchserve:latest)
 
 
-echo "[Copying model weights+code]"
+echo "[INFO] Copying model weights+code"
 
 docker cp $MODEL_WEIGHTS_PATH $CONTAINER_ID:$CONTAINER_MODEL_WEIGHTS_PATH
 docker cp $MODEL_CODE_PATH $CONTAINER_ID:$CONTAINER_CODE_PATH
 
-echo "[Compiling model]"
+echo "[INFO] Compiling model"
 docker exec $CONTAINER_ID \
     torch-model-archiver \
         --model-name $CONTAINER_MODEL_NAME \
@@ -52,10 +66,10 @@ docker exec $CONTAINER_ID \
         --extra-files /home/model-server/examples/image_classifier/index_to_name.json \
         --handler image_classifier
 
-echo "[Stopping conversion container]"
+echo "[INFO] Stopping conversion container"
 docker stop -t 0 $CONTAINER_ID
 
-echo "[Start serving]"
+echo "[INFO] Start serving"
 docker run --rm -it \
     -p 8080:8080 \
     -p 8081:8081 \
@@ -65,6 +79,6 @@ docker run --rm -it \
     -v $DIR/serve/examples:/home/model-server/examples \
     torchserve:latest torchserve --start --ncs --model-store $CONTAINER_STORE_PATH --models $CONTAINER_MODEL_NAME.mar
 
-echo "[Cleaning up]"
+echo "[INFO] Cleaning up"
 
 rm -rf $TEMP_FOLDER
