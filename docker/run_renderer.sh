679807e798e591ea1d31d5ecf7102e4745ecf18b
hadisalman <hadicsalman@gmail.com>
2020-10-31 12:17:15 +0000

    update docker

--- docker/run_renderer.sh
+++ docker/run_renderer.sh
@@ -3,13 +3,11 @@
 code_folder=$(dirname "$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )")
 env_folder=$(realpath $1)
 models_folder=$(realpath $2)
-output_folder=$(realpath $3)
 
 echo $SCRIPTPATH
 
 docker run -it --network=host --ipc=host --rm  \
     --mount type=bind,source="$code_folder",target=/code \
-    --mount type=bind,source="$env_folder",target=/data/environments \
-    --mount type=bind,source="$models_folder",target=/data/models \
-    --mount type=bind,source="$output_folder",target=/output \
+    --mount type=bind,source="$env_folder",target=/data/blender_environments \
+    --mount type=bind,source="$models_folder",target=/data/blender_models \
     sandbox bash
