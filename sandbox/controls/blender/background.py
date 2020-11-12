72330c61e8a6509a80a5c7a465d8e7e2fc44a2a0
Guillaume Leclerc <leclerc@mit.edu>
2020-11-12 03:18:54 -0500

    Add support for HDRI backgrounds

--- sandbox/controls/blender/background.py
+++ sandbox/controls/blender/background.py
@@ -14,6 +14,9 @@ class BackgroundControl(BaseControl):
     discrete_dims = {}
 
     def apply(self, img, H, S, V):
+        import bpy
+
+        bpy.context.scene.render.film_transparent = True
 
         alpha = img[:, :, 3:].astype(float) / 255
         img = img[:, :, :3] * alpha + 255 * (1 - alpha) * np.array(hsv_to_rgb(H, S, V))[None, None]
