accab4851029b054ff55067b6ea36bbfa8b6d35d
Andrew Ilyas <ailyas@mit.edu>
2020-12-09 19:22:35 -0500

    random search and post control fix

---
+++ sandbox/policies/random_search.py
@@ -0,0 +1,31 @@
+import numpy as np
+from itertools import product
+# Vis
+from robustness.tools import vis_tools
+from matplotlib import pyplot as plt
+import torch as ch
+
+class RandomSearchPolicy:
+    def __init__(self, continuous_dim, discrete_sizes, samples, seed=None):
+        """
+            Pick a total number of samples randomly
+        """
+        self.continuous_dim = continuous_dim
+        self.discrete_sizes = discrete_sizes
+        self.samples = samples
+        self.seed = seed
+
+    def hint_scheduler(self):
+        return 1, self.samples 
+
+    def run(self, render_and_send):
+        rng = np.random.default_rng(self.seed)
+        result = []
+        for _ in range(self.samples):
+            continuous_instance = rng.random(self.continuous_dim)
+            discrete_instance = [rng.integers(low=0, high=n) for n in self.discrete_sizes]
+            result.append((continuous_instance, discrete_instance))
+
+        images, logits, is_correct = render_and_send(result)
+
+Policy = RandomSearchPolicy
